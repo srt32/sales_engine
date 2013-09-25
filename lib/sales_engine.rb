e76e09ba73227dedbd50ae40f15ca2929de9a6ef
Simon Taranto <simon.taranto@gmail.com>
2013-09-25 16:32:46 -0600

    continue fixing file name issues

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,13 +1,16 @@
 require_relative 'customer_repository'
 require_relative 'customer'
-#require_relative 'invoice_repository'
+require_relative 'invoice_repository'
 require_relative 'invoice'
 require_relative 'merchant_repository'
 require_relative 'merchant'
-#require_relative 'transaction_repository'
+require_relative 'transaction_repository'
 require_relative 'transaction'
 require_relative 'invoice_item_repository'
 require_relative 'invoice_item'
+#require_relative 'item_repository'
+#require_relative 'item'
+
 
 class SalesEngine
 
@@ -31,15 +34,15 @@ class SalesEngine
     InvoiceRepository.new(filepath)
   end
 
-  def item_repository(filepath = "")
+  def item_repository(filepath = "./data/items.csv")
     ItemRepository.new(filepath)
   end
 
-  def merchant_repository(filepath = "")
+  def merchant_repository(filepath = "./data/merchants.csv")
     MerchantRepository.new(filepath)
   end
 
-  def transaction_repository(filepath = "")
+  def transaction_repository(filepath = "./data/transactions.csv")
     TransactionRepository.new(filepath)
   end
 
