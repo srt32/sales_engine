38b7f4f461bfb4de3bacccd411588346dcc52a12
Simon Taranto <simon.taranto@gmail.com>
2013-09-25 15:03:57 -0600

    begin changes to make spec_harness work

--- lib/sales_engine.rb
+++ lib/sales_engine.rb
@@ -1,7 +1,20 @@
+require_relative 'customer_repository'
+require_relative 'customer'
+#require_relative 'invoice_repository'
+require_relative 'invoice'
+require_relative 'merchant_repository'
+require_relative 'merchant'
+#require_relative 'transaction_repository'
+require_relative 'transaction'
+
 class SalesEngine
 
-  def startup
+  def initialize(data = './data')
+ 
+  end
 
+  def startup
+    
   end
 
   def customer_repository(filepath= "./data/customers.csv")
