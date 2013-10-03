250df7d784a3a37cd9c9824a3e1efd731e77c512
Simon Taranto <simon.taranto@gmail.com>
2013-10-03 16:31:54 -0600

    wip, dynamic group_by not working

--- lib/find_methods.rb
+++ lib/find_methods.rb
@@ -2,8 +2,19 @@ module FindMethods
 
   def create_finder_methods(attributes_string)
     attributes_string.each do |attribute|
+      define_method("grouped_by_#{attribute}") do 
+        if instance_variable_defined? "@grouped_by_#{attribute}"
+          instance_variable_get("@grouped_by_#{attribute}")
+        else
+          instance_variable_set("@grouped_by_#{attribute}",all.group_by{|object| object.send(attribute)})
+        end
+      end
+    end
+
+    attributes_string.each do |attribute|
       define_method("find_by_#{attribute}") do |criteria|
-        all.find{|c| c.send(attribute).to_s == criteria.to_s}
+        #all.find{|c| c.send(attribute).to_s == criteria.to_s}
+        self.class.send("grouped_by_#{attribute}")[criteria.to_s] || []
       end
     end
     
@@ -12,6 +23,7 @@ module FindMethods
         all.find_all{|c| c.send(attribute).to_s == criteria.to_s}
       end
     end
+
   end
 
 end
